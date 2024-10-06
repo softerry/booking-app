646ae093356377e5c5c7afd2da4568190731f59b
Terry <55205907+softerry@users.noreply.github.com>
2024-10-06 17:02:26 +0100

    added icons and toggle hide and show password

--- src/login.js
+++ src/login.js
@@ -1,23 +1,61 @@
 import React from "react";
+import {Icon} from 'react-icons-kit'
+import {facebook} from 'react-icons-kit/icomoon/facebook'
+import {google} from 'react-icons-kit/icomoon/google'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faUser} from '@fortawesome/free-solid-svg-icons';
+import { faKey } from "@fortawesome/free-solid-svg-icons";
+import { faEye } from '@fortawesome/free-regular-svg-icons';
+import { faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 
 function Login(){
+    // create  state to hide and show password
+    const [pass, setPassword] = useState('password');
+    const [icon, setIcon] = useState(faEyeSlash);
+
+    // create a toggle click icon to show and hide password
+    const toggleIcon = () =>{
+        if(pass === 'password'){
+            setPassword('text');
+            setIcon(faEye);
+        }else{
+            setPassword('password');
+            setIcon(faEyeSlash);
+        }
+    }
     return(
-        <div className='login-container'>
-            <form className="login-form">
+        <div id="main-login"> 
+             <div className='login-container'>
+                {/* // login form */}
+                 <form className="login-form">
                 <h2>Login</h2>
+                <div className="login-social-media-icon">
+                    <a href="#" className="media-icon"><Icon size={'20px'} icon={facebook}/></a>
+                    <a href="#" className="media-icon"><Icon size={'20px'} icon={google}/></a>
+                </div>
+                       <p className="login-option">-- or -- </p>
+                        
                 <div className="input-field">
+                    <span className="user-icon"><FontAwesomeIcon icon={faUser} /></span>
                     <input type="text" id='username' name="username" placeholder="E-mail"required/>
                 </div>
                 <div className="input-field">
-                    <input type="password" id='password' name='password' placeholder="Password" required/>
+                    <span className="password-key"><FontAwesomeIcon icon={faKey}/></span>
+                    <input type={pass} id='password' name='password' placeholder="Password" required/>
+                    <span className="password-eye"><FontAwesomeIcon icon={icon} onClick={toggleIcon}/></span>
                 </div>
-                <button type='submit'>Login</button>
+                <div className="checkbox">
+                    <input type="checkbox" id="login-checkbox"/><label>Remember Me</label>
+                </div>
+                           <button id='login-btn'type='submit'>Login</button>
                 <p className="signup-text">Don't have an account? <a href="#">sign up</a></p>
-
             </form>
-
         </div>
+      
+        </div>
+       
     )
 }
 
